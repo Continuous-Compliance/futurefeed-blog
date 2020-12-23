7ab7bcdbc78f0158ec23f3acdad97f789de3b3c2
vassilis <vassilis@codefully.io>
2020-12-23 16:05:16 +0200

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-material-ui`,
-    `gatsby-theme-material-ui`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
